2fbc4f312f70817d667ae91fb141880dee019196
realrihard <optobeats@gmail.com>
2023-03-16 07:02:33 +0200

    Renew build with admin files

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'],
+            input: ['resources/scss/admin.scss', 'resources/scss/app.scss', 'resources/js/app.js', 'resources/js/admin.js'],
             refresh: true,
         }),
         vue(),
